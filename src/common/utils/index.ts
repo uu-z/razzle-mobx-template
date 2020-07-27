a9b195ed9a94cf2f6007e3860588bd2f498ef49b
uuz <ncysatnaf@gmail.com>
2020-07-27 20:16:18 +0800

    clean up code

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,9 +1,7 @@
 import { Helper } from "./helper";
 import { Env } from "./env";
-import { eventBus } from "./eventBus";
 
 export const utils = {
   helper: new Helper(),
   env: new Env(),
-  eventBus,
 };
